72acfb99ee1c42bdf2497666dcce32403c6bb233
Junhao Lai <laix1449@yahoo.com>
2022-10-22 19:34:58 -0700

    revised

--- circleArea.c
+++ circleArea.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+#define PI 3.14
+
 int main()
 {
-  printf("Project 1");
+  double area = 0.0, radius = 0.0;
+  printf("Enter radius: ");
+  scanf("%lf",&radius);
+  area = PI * radius * radius;
+  printf("the area is %lf\n",area);
   return 0;
+
 }
